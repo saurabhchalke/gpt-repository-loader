0826aaf5f5c7299bd3af35dbf79ba9fd71b8435d
Saurabh Chalke <saurabhchalke@gmail.com>
2023-06-08 03:03:29 +0530

    Make changes to the default preamble

--- gpt_repository_loader.py
+++ gpt_repository_loader.py
@@ -87,9 +87,14 @@ if __name__ == "__main__":
                 preamble_text = pf.read()
                 output_file.write(f"{preamble_text}\n")
         else:
-            output_file.write(
-                "The following text is a Git repository with code. The structure of the text are sections that begin with ----, followed by a single line containing the file path and file name, followed by a variable amount of lines containing the file contents. The text representing the Git repository ends when the symbols --END-- are encounted. Any further text beyond --END-- are meant to be interpreted as instructions using the aforementioned Git repository as context.\n"
-            )
+            if only_filepath:
+                output_file.write(
+                    "The following text is a Git repository with code. The structure of the text are sections that begin with ----, followed by a single line containing the file path and file name. The text representing the Git repository ends when the symbols --END-- are encounted. Any further text beyond --END-- are meant to be interpreted as instructions using the aforementioned Git repository as context.\n"
+                )
+            else:
+                output_file.write(
+                    "The following text is a Git repository with code. The structure of the text are sections that begin with ----, followed by a single line containing the file path and file name, followed by a variable amount of lines containing the file contents. The text representing the Git repository ends when the symbols --END-- are encounted. Any further text beyond --END-- are meant to be interpreted as instructions using the aforementioned Git repository as context.\n"
+                )
         if only_filepath:
             process_repository_only_filepath(repo_path, ignore_list, output_file)
         else:
